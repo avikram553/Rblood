42633151ed56445ca1ba97b8c6fa56a6cd0064fc
avikram553 <avikram553@gmail.com>
2021-06-23 13:44:34 +0530

    Changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 sys.setrecursionlimit(10**6) 
 from flask import Flask,request,render_template,g,session,flash
 from flask import Flask
-from flaskext.mail import Mail, Message
+from flask_mail import Mail, Message
 from sqlalchemy.orm import sessionmaker
 import pyodbc
 import urllib
